405aaac547b3f803e6b018cf19980d9fabebdab2
HuDK <1369783326@qq.com>
2022-10-02 00:18:49 +0800

    `update`

--- app/Http/Controllers/Api/ApiMailController.php
+++ app/Http/Controllers/Api/ApiMailController.php
@@ -11,7 +11,7 @@ class ApiMailController extends Controller
     public function sendEmail(Request $request)
     {
         if($request->choose==1){
-            $text = '小雅如同意啦"，'.date("Y年m月d日 H:i:s") . '这是一个充满幸福而特别时间。';
+            $text = '可爱的小雅如同意啦，'.date("Y年m月d日 H:i:s") . '这是一个充满幸福而特别时间，一定要记住哦！';
         }else{
             $text = '李雅如的选择是："否"';
         }
