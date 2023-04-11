9bf7b9ee25db6c23ec8fa751881f8ef26a72a55e
HuDK <1369783326@qq.com>
2023-04-11 14:54:21 +0800

    `update`

--- app/Http/Controllers/Api/ApiSmsController.php
+++ app/Http/Controllers/Api/ApiSmsController.php
@@ -43,6 +43,7 @@ class ApiSmsController extends Controller
      */
     public function sendVerificationCode(Request $request)
     {
+        // 123
         $this->validate($request, [
             'useType' => 'required',
             'phone' => 'required|regex:/^1[345789][0-9]{9}$/',
